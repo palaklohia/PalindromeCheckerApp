8cbaa89c60e673b6c7f0f43a5d2693263eef6b3e
palaklohia <palaklohiyabth2005@gmail.com>
2026-03-09 15:53:15 +0530

    UC5:Stack Palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,27 +1,28 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String text = "madam";
+        Stack<Character> stack = new Stack<>();
 
-        // Convert string to character array
-        char[] chars = text.toCharArray();
+        // Push characters into stack
+        for (int i = 0; i < text.length(); i++) {
+            stack.push(text.charAt(i));
+        }
 
-        int start = 0;
-        int end = chars.length - 1;
         boolean isPalindrome = true;
 
-        // Two-pointer comparison
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        // Pop characters and compare
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-        // Display result
+        // Print result
         System.out.println("Input text : " + text);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
